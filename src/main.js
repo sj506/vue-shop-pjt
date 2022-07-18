6ef392833e4ec0271da7f3b66148ef4a1065db74
Seung-jae <tmdwo5040@gmail.com>
2022-07-18 10:47:09 +0900

    카카오 로그인

--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ import mixins from './mixins';
 
 createApp(App).mixin(mixins).use(store).use(router).mount('#app');
 
-window.Kakao.init('c77f9d76956a98cf9fffb34650220bd5'); // 다운로드한 kakao.js에서 내가 원하는 값을 할당해 넣어주겠다. init메소드가 있는데 여기에 할당해서 넣어줄것이다.
+window.Kakao.init('03e7f27c977abda364ebc79b908f3a56'); // 다운로드한 kakao.js에서 내가 원하는 값을 할당해 넣어주겠다. init메소드가 있는데 여기에 할당해서 넣어줄것이다.
 // init('kakao의 js 키값.')
